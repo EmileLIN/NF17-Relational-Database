c4edf31648fe1690174ac21d8e31dd45f9b51db5
EmileLIN <shuhanLin@shuhantekiMacBook-Pro.local>
2015-06-10 21:56:15 +0200

    update

--- public_html/php/connect.php
+++ public_html/php/connect.php
@@ -1,9 +1,9 @@
 <?php
 	function fConnect(){
-		$vHost="xxxxxxxxxxx";
-		$vPort="xxxx";
-		$vDbname="xxxxxxxxxxx";
-		$vUser="xxxxxxxxx";
+		$vHost="xxxxxxxx";
+		$vPort="xxxxxxx";
+		$vDbname="xxxxxxxxx";
+		$vUser="xxxxxxxx";
 		$vPassword="xxxxxxxx";
 		$vConn = pg_connect("host=$vHost port=$vPort dbname=$vDbname user=$vUser password=$vPassword");
 		if($vConn != FALSE)
